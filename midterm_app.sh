31cb4dbf707a4e861986261e87de57f58192f050
qammisip <qammisip@tip.edu.ph>
2022-11-17 04:28:39 +0000

    Updates for Jenkins

--- midterm_app.sh
+++ midterm_app.sh
@@ -13,10 +13,10 @@ echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY ./static /home/myapp/static" >> tempdir/Dockerfile
 echo "COPY ./templates /home/myapp/templates" >> tempdir/Dockerfile
 echo "COPY midterm_app.py /home/myapp" >> tempdir/Dockerfile
-echo "EXPOSE 5050" >> tempdir/Dockerfile
+echo "EXPOSE 8080" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/midterm_app.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t midtermprojapp .
-docker run -t -d -p 5050:5050 --name midtermprojrun midtermprojapp
+docker run -t -d -p 8080:8080 --name midtermprojrun midtermprojapp
 docker ps -a
--- tempdir/midterm_app.py
+++ tempdir/midterm_app.py
@@ -10,4 +10,4 @@ def main():
     return render_template("login.html")
 
 if __name__== "__main__":
-    sample.run(host="0.0.0.0", port=8080)
+    sample.run(host="0.0.0.0", port=5050)
