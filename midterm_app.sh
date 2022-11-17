2b6dcb2d0beecf6ed9668e369052776f07fb8d7b
qammisip <qammisip@tip.edu.ph>
2022-11-17 04:12:23 +0000

    Updates

--- midterm_app.sh
+++ midterm_app.sh
@@ -4,7 +4,7 @@ mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
 
-cp design_app.py tempdir/.
+cp midterm_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
@@ -12,11 +12,11 @@ echo "FROM python" > tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY ./static /home/myapp/static" >> tempdir/Dockerfile
 echo "COPY ./templates /home/myapp/templates" >> tempdir/Dockerfile
-echo "COPY design_app.py /home/myapp" >> tempdir/Dockerfile
+echo "COPY midterm_app.py /home/myapp" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
-echo "CMD python3 /home/myapp/design_app.py" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/midterm_app.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t midtermprojapp .
-docker run -t -d -p 5050:5050 --name midtermprojapp midtermprojapp
+docker run -t -d -p 5050:5050 --name midtermprojrun midtermprojapp
 docker ps -a
